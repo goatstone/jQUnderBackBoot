e08206afe0e71b28a5354f5ec51925737752fbae
goatstone <goatstone@gmail.com>
2014-04-23 14:10:43 -0700

    Adding second View.

--- js/goatstone/css_color_bb.js
+++ js/goatstone/css_color_bb.js
@@ -23,20 +23,59 @@
             'mousemove': 'onMove',
             'mouseup': 'onMouseUp',
             'mouseout': 'onMouseOut',
-            'keyup #query': 'searchColors'
+            'keyup #query': 'searchColors',
+            'click input[name=selection_mode]': 'selectMode'
+
         },
         isDragging: false,
         xOffSet: 0,
         yOffSet: 0,
+        mode: "elements",
+        selectMode: function (e) {
+            console.log("selectMode");
+            console.log(e);
+            this.mode = e.currentTarget.value;
+            console.log(this.mode);
+            this.render();
+
+        },
         onResize: function () {
             this.render();
         },
+        render: function () {
+            if (this.mode === "elements") {
+                renderElements.apply(this);
+            }
+            else if (this.mode === "props") {
+                renderProps.apply(this);
+            }
+            else if (this.mode === "vals") {
+                renderColors.apply(this);
+            }
+        },
+        searchColors: function (e) {
+            if (this.mode !== "vals") {
+//                return;
+            }
+            e.stopPropagation();
+            e.preventDefault();
+            console.log(e)
+            var query = this.$queryInput.val();
+            var newArr = [];
+            newArr = _.filter(css_colors_names, function (el) {
+                return new RegExp(query).test(el.name);
+            });
+            this.collection.reset(newArr);
+            return false;
+        },
         initialize: function () {
-            _.bindAll(this, 'render', "onMouseOut" ,"onMouseUp" ,"onMouseDown", "onMove", "searchColors");
+            _.bindAll(this, 'render', "onMouseOut", "onMouseUp",
+                "onMouseDown", "onMove", "searchColors", "selectMode");
 
             this.$queryInput = $(this.el).find('#query');
-            this.$display = $('#color_names');
-            this.$queryPanel = $("#search_color");
+//            this.$display = $('#color_names');
+            this.$display = $('#main_display');
+            this.$queryPanel2 = $("#search_color");
 
             this.collection = new Colors();
             this.collection.bind('reset', this.render); // collection event binder
@@ -54,82 +93,21 @@
         onMouseDown: function (e) {
             this.isDragging = true;
             var yOffSet = 0;
-            var offSetDiv = this.$queryPanel.offset();
+            var offSetDiv = this.$queryPanel2.offset();
             this.xOffSet = e.clientX - offSetDiv.left;
             this.yOffSet = e.clientY - offSetDiv.top;
         },
         onMove: function (e) {
+            console.log("onMove");
+            console.log(this.isDragging);
+
             if (this.isDragging) {
                 var offSet = 20;
                 var xOffSet = 0;
-                this.$queryPanel.css({"left": e.clientX - this.xOffSet });
-                this.$queryPanel.css({"top": e.clientY - this.yOffSet });
+                this.$queryPanel2.css({"left": e.clientX - this.xOffSet });
+                this.$queryPanel2.css({"top": e.clientY - this.yOffSet });
 
             }
-        },
-        render: function () {
-            var $this = this;
-            this.$display.html("");
-            var i = 1;
-            var j = 0; //x
-            var k = 0; //y
-            var wInc = 51;
-            var hInc = 50;
-            var screenWidth = $(window).width();
-            var screenHeight = $(window).height();
-            var rowCount = 15;
-            var pixelCount = 0;
-            var tileCount = 0;
-            var pixelPerTile = 0;
-            var tileSideDim = 0;
-            var colCount = 0
-
-            pixelCount = screenWidth * screenHeight;
-            tileCount = this.collection.length;
-            pixelPerTile = pixelCount / tileCount;
-            tileSideDim = Math.floor(Math.sqrt(pixelPerTile));
-            rowCount = Math.floor(screenWidth / tileSideDim);
-            colCount = Math.ceil(tileCount / rowCount);
-
-            wInc = Math.floor(screenWidth / rowCount);
-            hInc = Math.floor(screenHeight / colCount);
-            _(this.collection.models).each(function (color) { // in case collection is not empty
-                var divEl = $("<div>");
-                var top = 0, left = 0;
-                left = wInc * (j);
-                top = hInc * (k);
-
-                divEl.css(
-                    {
-                        "background-color": color.get("name"),
-                        "left": left,
-                        "top": top,
-                        "width": wInc,
-                        "height": hInc
-                    });
-                if (i % rowCount === 0) { // make a new row
-                    k++;
-                    j = 0;
-                } else {
-                    j++;
-                }
-                i++;
-
-//                divEl.text(color.get("name"))
-                $this.$display.append(divEl);
-            }, this);
-        },
-        searchColors: function (e) {
-            e.stopPropagation();
-            e.preventDefault();
-            console.log(e)
-            var query = this.$queryInput.val();
-            var newArr = [];
-            newArr = _.filter(css_colors_names, function (el) {
-                return new RegExp(query).test(el.name);
-            });
-            this.collection.reset(newArr);
-            return false;
         }
     });
 
@@ -141,4 +119,67 @@
 
 })(jQuery);
 
+function renderElements() {
+    var $this = this;
+    this.$display.html("els");
+//    this.$display.append("hello");
+}
+function renderProps() {
+    var $this = this;
+    this.$display.html("props");
+//    this.$display.append("hello");
+}
+
+function renderColors() {
+    var $this = this;
+    this.$display.html("");
+    var i = 1;
+    var j = 0; //x
+    var k = 0; //y
+    var wInc = 51;
+    var hInc = 50;
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
+    var rowCount = 15;
+    var pixelCount = 0;
+    var tileCount = 0;
+    var pixelPerTile = 0;
+    var tileSideDim = 0;
+    var colCount = 0
+
+    pixelCount = screenWidth * screenHeight;
+    tileCount = this.collection.length;
+    pixelPerTile = pixelCount / tileCount;
+    tileSideDim = Math.floor(Math.sqrt(pixelPerTile));
+    rowCount = Math.floor(screenWidth / tileSideDim);
+    colCount = Math.ceil(tileCount / rowCount);
+
+    wInc = Math.floor(screenWidth / rowCount);
+    hInc = Math.floor(screenHeight / colCount);
+    _(this.collection.models).each(function (color) { // in case collection is not empty
+        var divEl = $("<div>");
+        var top = 0, left = 0;
+        left = wInc * (j);
+        top = hInc * (k);
+
+        divEl.css(
+            {
+                "background-color": color.get("name"),
+                "left": left,
+                "top": top,
+                "width": wInc,
+                "height": hInc
+            });
+        if (i % rowCount === 0) { // make a new row
+            k++;
+            j = 0;
+        } else {
+            j++;
+        }
+        i++;
+
+//                divEl.text(color.get("name"))
+        $this.$display.append(divEl);
+    }, this);
+}
 var colorView;
