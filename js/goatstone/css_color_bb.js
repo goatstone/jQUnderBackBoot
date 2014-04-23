f19d5828e19771ef446487f08b94d8b2bd7dcb0d
goatstone <goatstone@gmail.com>
2014-04-22 21:30:46 -0700

    Setting the colors to fill the viewport.

--- js/goatstone/css_color_bb.js
+++ js/goatstone/css_color_bb.js
@@ -6,7 +6,7 @@
             name: 'blue',
             r: 0,
             g: 0,
-            b :255
+            b: 255
         }
     });
 
@@ -27,7 +27,7 @@
         xOffSet: 0,
         yOffSet: 0,
         initialize: function () {
-            _.bindAll(this, 'render' );
+            _.bindAll(this, 'render');
 
             this.$queryInput = $(this.el).find('#query');
             this.$display = $('#color_names');
@@ -38,60 +38,78 @@
             this.collection.reset(css_colors_names);
             this.$queryInput.focus();
         },
-        onMouseOut: function(){
-            console.log("on onMouseOut");
-            //this.isDragging = false;
+        onMouseOut: function () {
             this.$queryInput.focus();
         },
-        onMouseUp: function(){
-            console.log("on onMouseUp");
+        onMouseUp: function () {
             this.isDragging = false;
             this.$queryInput.focus();
 
         },
-        onMouseDown : function(e){
-            console.log("onMouseDown")
-            this.isDragging = true;
+        onMouseDown: function (e) {
+             this.isDragging = true;
             var yOffSet = 0;
-            var offSetDiv= this.$queryPanel.offset();
-            this.xOffSet =   e.clientX -offSetDiv.left ;
-            this.yOffSet =   e.clientY -offSetDiv.top ;
-            console.log(this.offSetDiv);
-            console.log(e.clientX);
-//            console.log(this.xOffSet);
+            var offSetDiv = this.$queryPanel.offset();
+            this.xOffSet = e.clientX - offSetDiv.left;
+            this.yOffSet = e.clientY - offSetDiv.top;
         },
-        onMove : function(e){
-            console.log("on move");
-//            console.log(e.offsetX);
-            if(this.isDragging){
+        onMove: function (e) {
+             if (this.isDragging) {
                 var offSet = 20;
                 var xOffSet = 0;
-                console.log("is dragging");
-                console.log(this.xOffSet );
-                console.log(e.clientX);
-
-//                $(this.el).css({"top": e.clientY + offSet });
                 this.$queryPanel.css({"left": e.clientX - this.xOffSet });
                 this.$queryPanel.css({"top": e.clientY - this.yOffSet });
 
             }
-            //this.isDragging = truel;
         },
         render: function () {
-             var $this = this;
+            var $this = this;
             this.$display.html("");
+            var i = 1;
+            var j = 0; //x
+            var k = 0; //y
+            var wInc = 51;
+            var hInc = 50;
+            var screenWidth = $(window).width();
+            var screenHeight = $(window).height();
+            var rowCount = 15;
+            var pixelCount = 0;
+            var tileCount = 0;
+            var pixelPerTile = 0;
+            var tileSideDim = 0;
+            var colCount = 0
 
+            pixelCount = screenWidth * screenHeight;
+            tileCount = this.collection.length;
+            pixelPerTile = pixelCount / tileCount;
+            tileSideDim = Math.floor(Math.sqrt(pixelPerTile));
+            rowCount = Math.floor(screenWidth / tileSideDim);
+            colCount = Math.ceil(tileCount / rowCount);
+
+            wInc = Math.floor(screenWidth / rowCount);
+            hInc = Math.floor(screenHeight / colCount);
             _(this.collection.models).each(function (color) { // in case collection is not empty
                 var divEl = $("<div>");
-                divEl.css (
+                var top = 0, left = 0;
+                left = wInc * (j);
+                top = hInc * (k);
+                divEl.css(
                     {
-                        "background-color":color.get("name"),
-                        "left":200,
-                        "right":200,
-                        "width":20,
-                        "height":20
-                });
-                divEl.text( color.get("name") )
+                        "background-color": color.get("name"),
+                        "left": left,
+                        "top": top,
+                        "width": wInc,
+                        "height": hInc
+                    });
+                if (i % rowCount === 0) { // make a new row
+                    k++;
+                    j = 0;
+                } else {
+                    j++;
+                }
+                i++;
+
+//                divEl.text(color.get("name"))
                 $this.$display.append(divEl);
             }, this);
         },
@@ -105,9 +123,9 @@
                 return new RegExp(query).test(el.name);
             });
             this.collection.reset(newArr);
-            return false ;
+            return false;
         }
-     });
+    });
 
     var colorView = new ColorView();
 
