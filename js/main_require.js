1fb7e20c107c73d80304e97dea763cff2c9ca0e0
goatstone <goatstone@gmail.com>
2014-05-08 13:01:12 -0700

    Set up functionality for SelectorView and correct the model.

--- js/main_require.js
+++ js/main_require.js
@@ -1,13 +1,12 @@
 //the require library is configuring paths
 require.config({
     paths: {
-        //tries to load jQuery from Google's CDN first and falls back
-        //to load locally
+        //tries to load jQuery from Google's CDN first and falls back to load locally
         "jquery": ["http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
             "libs/jquery/jquery"],
         "underscore": "../bower_components/underscore/underscore-min",
         "backbone": "../bower_components/backbone/backbone-min",
-        "element_config_model":"./goatstone/models/ElementConfig"
+        "element_config_model": "./goatstone/models/ElementConfig"
     },
     shim: {
         "backbone": {
@@ -22,32 +21,8 @@ require.config({
 });
 //requiring the scripts in the first argument and then passing the library namespaces into a callback
 //you should be able to console log all of the callback arguments
-require(['jquery', 'underscore', 'backbone', 'goatstone/app'], function(jquery, _, Backbone, App){
+require(['jquery', 'underscore', 'backbone', 'goatstone/app'], function (jquery, _, Backbone, App) {
 
-    //    console.log(G)
-//    var ElementConfigModel = Backbone.Model.extend({
-//        defaults: {
-//            name: "Shared Info",
-//            selectionMode: "element",
-//            element: "div",
-//            property: "color",
-//            properties:
-//            {"color": "green", "backgroundColor": "red", "text": "hello el" },
-//            value: "red",
-//            text: "hello html",
-//            dummyProp:0
-//        }
-//    });
-//
-//    var List = Backbone.Collection.extend({
-//        model: ElementConfigModel
-//    });
-//
-//    var elementConfigModel = new ElementConfigModel();
-//    elementConfigModel.set("name", "G Model !!!")
-//    elementConfigModel.bind('change', function () {
-//        console.log("G model changed !!!!!! ")
-//    })
+    new App();
 
-    new App( );
 });
